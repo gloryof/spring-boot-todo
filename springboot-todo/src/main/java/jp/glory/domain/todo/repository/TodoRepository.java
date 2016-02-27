79f926778d9ba9a5bc2f1be8f8f64e59ad9ee5a1
Junki Yamada <geek.junkey@gmail.com>
2016-02-27 10:53:37 +0900

    Todosに戻りの型を変更。

--- springboot-todo/src/main/java/jp/glory/domain/todo/repository/TodoRepository.java
+++ springboot-todo/src/main/java/jp/glory/domain/todo/repository/TodoRepository.java
@@ -1,26 +1,30 @@
 package jp.glory.domain.todo.repository;
 
-import java.util.List;
-
 import jp.glory.domain.todo.entity.Todo;
+import jp.glory.domain.todo.entity.Todos;
 import jp.glory.domain.todo.value.TodoId;
 import jp.glory.domain.user.value.UserId;
 
 /**
  * TODOのリポジトリ.
+ * 
  * @author Junki Yamada
  */
 public interface TodoRepository {
     /**
      * ユーザIDをキーにTODOを取得する.
-     * @param userId ユーザID
+     * 
+     * @param userId
+     *            ユーザID
      * @return TODOリスト
      */
-    List<Todo> findBy(final UserId userId);
+    Todos findBy(final UserId userId);
 
     /**
      * TODOを保存する.
-     * @param todo TODOエンティティ
+     * 
+     * @param todo
+     *            TODOエンティティ
      * @return TODOのID
      */
     TodoId save(final Todo todo);
