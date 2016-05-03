b2da41b9af630c77788d836de3e44c855587b85f
Junki Yamada <geek.junkey@gmail.com>
2016-05-03 13:34:23 +0900

    DBの構築に必要な情報を追加

---
+++ springboot-todo/ddl/02-create-table-todo.sql
@@ -0,0 +1,17 @@
+CREATE TABLE todos (
+	todo_id BIGINT NOT NULL,
+	user_id BIGINT NOT NULL,
+	summary VARCHAR(20) NOT NULL,
+	completed BOOLEAN NOT NULL,
+	PRIMARY KEY(todo_id)
+);
+
+CREATE TABLE todos_detail (
+	todo_id BIGINT NOT NULL REFERENCES todos(todo_id),
+	memo TEXT NOT NULL,
+	PRIMARY KEY(todo_id)
+);
+
+CREATE INDEX idx_todos_user_id ON users (user_id);
+
+CREATE SEQUENCE todo_id_seq;
