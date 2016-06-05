584899b590d2424d36fd59367bd9663395aebbd0
Junki Yamada <geek.junkey@gmail.com>
2016-06-05 14:49:12 +0900

    パフォーマンスログ出力を追加

---
+++ springboot-todo/src/main/java/jp/glory/framework/interceptors/log/InterceptorSetting.java
@@ -0,0 +1,15 @@
+package jp.glory.framework.interceptors.log;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+/**
+ * インターセプターの設定
+ * @author Junki Yamada
+ *
+ */
+@Configuration
+@EnableAspectJAutoProxy
+public class InterceptorSetting {
+
+}
