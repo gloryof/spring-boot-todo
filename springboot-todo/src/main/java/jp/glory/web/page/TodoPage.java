924e9120fa0620c11bea34318ac29bb6569a9438
Junki Yamada <geek.junkey@gmail.com>
2016-06-21 21:51:16 +0900

    不要なパラメータを削除。

--- springboot-todo/src/main/java/jp/glory/web/page/TodoPage.java
+++ springboot-todo/src/main/java/jp/glory/web/page/TodoPage.java
@@ -1,14 +1,11 @@
 package jp.glory.web.page;
 
 import org.springframework.context.annotation.Scope;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.ModelAndView;
-
-import jp.glory.web.session.UserInfo;
 
 /**
  * TODOページ.
@@ -27,7 +24,7 @@ public class TodoPage {
      * @return TODOページ.
      */
     @RequestMapping
-    public ModelAndView view(@AuthenticationPrincipal final UserInfo userInfo) {
+    public ModelAndView view() {
 
         return new ModelAndView("todos/todos");
     }
