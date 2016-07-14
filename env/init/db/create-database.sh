613482056bcc88f34c9a9900d6bb72b9c7de4a86
Junki Yamada <geek.junkey@gmail.com>
2016-07-14 22:43:37 +0900

    ユーザ作成、エンコード周りを設定動作するようにした

--- env/init/db/create-database.sh
+++ env/init/db/create-database.sh
@@ -2,8 +2,19 @@
 
 set -e
 
+INIT_FLG_FILE=/var/lib/postgresql/data/execute-init
+
+if [ ! -e ${INIT_FLG_FILE} ]; then
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE USER "todo-user";
-    CREATE DATABASE "boot-todo";
+    CREATE USER "todo-user"
+      WITH PASSWORD 'boot-todo';
+    CREATE DATABASE "boot-todo"
+      WITH OWNER = "todo-user"
+        ENCODING = "UTF-8";
     GRANT ALL PRIVILEGES ON DATABASE "boot-todo" TO "todo-user";
 EOSQL
+else
+echo 'init file is exists.'
+fi
+
+touch ${INIT_FLG_FILE}
