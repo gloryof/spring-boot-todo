b4b1eb38716c1e7d14e0984b37e49ff0f3e4f08c
Junki Yamada <geek.junkey@gmail.com>
2016-07-16 10:18:26 +0900

    FlywayのSQLファイルのまとめる単位を間違えていたので修正

--- env/init/db/ddl/v1.0.0__init-tables.sql
+++ env/init/db/ddl/v1.0.0__init-tables.sql
@@ -1,3 +1,17 @@
+-- users関係のDDL
+CREATE TABLE users (
+	user_id BIGINT NOT NULL,
+	login_id VARCHAR(50) NOT NULL,
+	user_name VARCHAR(50) NOT NULL,
+	password TEXT NOT NULL,
+	PRIMARY KEY(user_id)
+);
+
+CREATE INDEX idx_users_login_id ON users (login_id);
+
+CREATE SEQUENCE user_id_seq;
+
+-- todos関係のDDL
 CREATE TABLE todos (
 	todo_id BIGINT NOT NULL,
 	user_id BIGINT NOT NULL,
