a8aca372ec23f84bd691f6d9bc80732902b66a13
Junki Yamada <geek.junkey@gmail.com>
2016-07-18 10:52:04 +0900

    コンテナごとにVOLUMEが指定されるためホスト側に固定のパスを指定するようにした

--- postgresql-docker/start.sh
+++ postgresql-docker/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -e TZ=Asia/Tokyo --name db01 -d -p 5432:5432 -it spring-todo:db01
+docker run -e TZ=Asia/Tokyo --name db01 -v /var/lib/sprint-todo/postgres/data:/var/lib/postgresql/data -d -p 5432:5432 -it spring-todo:db01
