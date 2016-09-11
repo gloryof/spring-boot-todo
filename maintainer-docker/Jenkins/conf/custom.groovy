4a8c73f3b71b0e3450acd1fc2243bdcf16724c14
Junki Yamada <geek.junkey@gmail.com>
2016-09-11 13:50:11 +0900

    苦労なく自動化できるところまでは自動化した。

--- maintainer-docker/Jenkins/conf/custom.groovy
+++ maintainer-docker/Jenkins/conf/custom.groovy
@@ -5,6 +5,6 @@ javaDesc.setInstallations( new hudson.model.JDK("Java", System.getenv() ["JAVA_H
 
 mvnDesc = Jenkins.instance.getExtensionList(hudson.tasks.Maven.DescriptorImpl.class)[0];
 mvnInstalls = (mvnDesc.installations as List);
-mvnInstalls.add(new hudson.tasks.Maven.MavenInstallation("maven3", System.getenv() ["INSTALLED_DIR"], []));
+mvnInstalls.add(new hudson.tasks.Maven.MavenInstallation("maven3", System.getenv() ["INSTALLE_DIR"], []));
 mvnDesc.installations = mvnInstalls;
 mvnDesc.save();
