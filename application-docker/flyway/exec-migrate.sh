64e13837c380103795db0514d8c107d6bb0f4845
Junki Yamada <geek.junkey@gmail.com>
2016-09-16 11:55:56 +0900

    db-migrationが動作していなかったので修正

--- application-docker/flyway/exec-migrate.sh
+++ application-docker/flyway/exec-migrate.sh
@@ -3,7 +3,6 @@ cd /var/lib/spring-todo/git/
 
 MIGRATE_URL=jdbc:postgresql://${TARGET_IP}:${TARGET_PORT}/${TARGET_DB}
 
-
 until psql -h "${TARGET_IP}" -d "${TARGET_DB}" -U "${DB_USER}" -c '\l'; do
     >&2 echo "Postgres is unavailable - sleeping"
     sleep 1
