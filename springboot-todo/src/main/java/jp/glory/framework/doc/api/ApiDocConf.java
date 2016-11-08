c8a1cfcb3914a9d65ea06a18ea9fe2db51d1fb78
Junki Yamada <geek.junkey@gmail.com>
2016-11-08 22:31:36 +0900

    不要なエンドポイントも対象になっていたのでWebAPIのみを拾うようにした。

--- springboot-todo/src/main/java/jp/glory/framework/doc/api/ApiDocConf.java
+++ springboot-todo/src/main/java/jp/glory/framework/doc/api/ApiDocConf.java
@@ -2,7 +2,9 @@ package jp.glory.framework.doc.api;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -23,6 +25,9 @@ public class ApiDocConf {
     @Bean
     public Docket apiDoc() {
 
-        return new Docket(DocumentationType.SWAGGER_2);
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                    .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                    .build();
     }
 }
