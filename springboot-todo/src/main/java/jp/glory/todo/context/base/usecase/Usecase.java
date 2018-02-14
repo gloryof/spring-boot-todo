74c11d2864325c112f098bcd3705dc11bf3df62a
Junki Yamada <geek.junkey@gmail.com>
2018-02-14 10:45:53 +0900

    ユースケースにステートはないのでシングルトンスコープとした

--- springboot-todo/src/main/java/jp/glory/todo/context/base/usecase/Usecase.java
+++ springboot-todo/src/main/java/jp/glory/todo/context/base/usecase/Usecase.java
@@ -5,13 +5,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.context.WebApplicationContext;
 
 @Service
-@Scope(value = WebApplicationContext.SCOPE_REQUEST)
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Transactional
